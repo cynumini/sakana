7273fb1931efe0c8a5419cac14ec4e00c2f9ecfc
cynumini <cynumini@proton.me>
2025-04-04 19:42:00 +0300

    add OpenGL 4.3 debug

--- src/root.zig
+++ src/root.zig
@@ -34,9 +34,11 @@ pub fn init(
     // cglfw.glfwInitHint(cglfw.GLFW_PLATFORM, cglfw.GLFW_PLATFORM_X11);
     if (c.glfwInit() != c.GLFW_TRUE) return error.GLFWInitError;
 
-    c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MAJOR, 3);
+    c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MAJOR, 4);
     c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MINOR, 3);
     c.glfwWindowHint(c.GLFW_OPENGL_PROFILE, c.GLFW_OPENGL_CORE_PROFILE);
+    // Enable debug;
+    c.glfwWindowHint(c.GLFW_OPENGL_DEBUG_CONTEXT, 0); // 1 = true, 0 = false
 
     const window = c.glfwCreateWindow(
         @intFromFloat(config.size[0]),
@@ -65,7 +67,53 @@ pub fn init(
 
     setClearColor(config.clear_color);
 
+    var flags: i32 = undefined;
+    c.glGetIntegerv(c.GL_CONTEXT_FLAGS, &flags);
+
+    if ((flags & c.GL_CONTEXT_FLAG_DEBUG_BIT) != 0) {
+        c.glEnable(c.GL_DEBUG_OUTPUT);
+        c.glEnable(c.GL_DEBUG_OUTPUT_SYNCHRONOUS);
+        c.glDebugMessageCallback(glDebugOutput, null);
+        c.glDebugMessageControl(c.GL_DONT_CARE, c.GL_DONT_CARE, c.GL_DONT_CARE, 0, null, c.GL_TRUE);
+    }
+
     return .{ .window = window };
+}
+
+export fn glDebugOutput(source: u32, @"type": u32, id: u32, severity: u32, length: i32, message: [*c]const u8, user_param: ?*const anyopaque) void {
+    _ = length;
+    _ = user_param;
+    if (id == 131169 or id == 131185 or id == 131218 or id == 131204) return;
+    std.debug.print("---------------\n", .{});
+    std.debug.print("Debug message ({}): {s}\n", .{ id, message });
+    switch (source) {
+        c.GL_DEBUG_SOURCE_API => std.debug.print("Source: API\n", .{}),
+        c.GL_DEBUG_SOURCE_WINDOW_SYSTEM => std.debug.print("Source: Window System\n", .{}),
+        c.GL_DEBUG_SOURCE_SHADER_COMPILER => std.debug.print("Source: Shader Compiler\n", .{}),
+        c.GL_DEBUG_SOURCE_THIRD_PARTY => std.debug.print("Source: Third Party\n", .{}),
+        c.GL_DEBUG_SOURCE_APPLICATION => std.debug.print("Source: Application\n", .{}),
+        c.GL_DEBUG_SOURCE_OTHER => std.debug.print("Source: Other\n", .{}),
+        else => std.debug.print("error.DebugUnknownSource\n", .{}),
+    }
+    switch (@"type") {
+        c.GL_DEBUG_TYPE_ERROR => std.debug.print("Type: Error\n", .{}),
+        c.GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR => std.debug.print("Type: Deprecated Behaviour\n", .{}),
+        c.GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR => std.debug.print("Type: Undefined Behaviour\n", .{}),
+        c.GL_DEBUG_TYPE_PORTABILITY => std.debug.print("Type: Portability\n", .{}),
+        c.GL_DEBUG_TYPE_PERFORMANCE => std.debug.print("Type: Performance\n", .{}),
+        c.GL_DEBUG_TYPE_MARKER => std.debug.print("Type: Marker\n", .{}),
+        c.GL_DEBUG_TYPE_PUSH_GROUP => std.debug.print("Type: Push Group\n", .{}),
+        c.GL_DEBUG_TYPE_POP_GROUP => std.debug.print("Type: Pop Group\n", .{}),
+        c.GL_DEBUG_TYPE_OTHER => std.debug.print("Type: Other\n", .{}),
+        else => std.debug.print("error.DebugUnknownType\n", .{}),
+    }
+    switch (severity) {
+        c.GL_DEBUG_SEVERITY_HIGH => std.debug.print("Severity: high\n", .{}),
+        c.GL_DEBUG_SEVERITY_MEDIUM => std.debug.print("Severity: medium\n", .{}),
+        c.GL_DEBUG_SEVERITY_LOW => std.debug.print("Severity: low\n", .{}),
+        c.GL_DEBUG_SEVERITY_NOTIFICATION => std.debug.print("Severity: notification\n", .{}),
+        else => std.debug.print("error.DebugUnknownSeverity\n", .{}),
+    }
 }
 
 pub fn deinit(self: *Self) void {
@@ -116,7 +164,7 @@ pub fn drawRectangle(position: Vector2, size: Vector2, color: Color) !void {
         c.GL_ARRAY_BUFFER,
         @sizeOf(f32) * vertices.len,
         @ptrCast(&vertices),
-        c.GL_STATIC_DRAW,
+        c.GL_DYNAMIC_DRAW,
     );
 
     c.glBindBuffer(c.GL_ELEMENT_ARRAY_BUFFER, EBO);
@@ -126,7 +174,7 @@ pub fn drawRectangle(position: Vector2, size: Vector2, color: Color) !void {
         c.GL_ELEMENT_ARRAY_BUFFER,
         @sizeOf(u32) * indices.len,
         @ptrCast(&indices),
-        c.GL_STATIC_DRAW,
+        c.GL_DYNAMIC_DRAW,
     );
 
     c.glVertexAttribPointer(0, 3, c.GL_FLOAT, c.GL_FALSE, 3 * @sizeOf(f32), @ptrFromInt(0));
