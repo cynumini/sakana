3baec745cb246e196d3d91d250c4641e2cd3b125
cynumini <cynumini@proton.me>
2026-03-17 21:28:09 +0200

    add front macro

--- include/SKN/array.h
+++ include/SKN/array.h
@@ -61,6 +61,27 @@
         array->len++;                                                                                                  \
     }
 
+#define DYNAMIC_ARRAY_IMPL_ADD_FRONT(ARRAY_TYPE, TYPE, NAME)                                                           \
+    static void NAME(Arena *arena, ARRAY_TYPE *array, TYPE value)                                                      \
+    {                                                                                                                  \
+        usize index = array->len;                                                                                      \
+        if (index >= array->capacity)                                                                                  \
+        {                                                                                                              \
+            if (array->capacity == 0)                                                                                  \
+                array->capacity = 8;                                                                                   \
+            else                                                                                                       \
+                array->capacity *= 2;                                                                                  \
+            array->items =                                                                                             \
+                (TYPE *)arena_realloc(arena, array->items, sizeof(TYPE) * index, sizeof(TYPE) * array->capacity);      \
+        }                                                                                                              \
+        for (usize i = index; i > 0; i--)                                                                              \
+        {                                                                                                              \
+            array->items[i] = array->items[i - 1];                                                                     \
+        }                                                                                                              \
+        array->items[0] = value;                                                                                       \
+        array->len++;                                                                                                  \
+    }
+
 #define DYNAMIC_ARRAY_IMPL_SWAP_REMOVE(ARRAY_TYPE, NAME)                                                               \
     /* Replace element at index with last element and reduce size */                                                   \
     static void NAME(ARRAY_TYPE *array, usize index)                                                                   \
